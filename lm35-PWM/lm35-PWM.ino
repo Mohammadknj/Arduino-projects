796b261b6049a9b5f38e6cd4dac253c1fae861f3
Mohammad <mohammad8mohammadi4@gmail.com>
2025-07-12 03:26:07 +0330

    buzz added. before changing 2nd clock by myself

--- lm35-PWM/lm35-PWM.ino
+++ lm35-PWM/lm35-PWM.ino
@@ -64,10 +64,10 @@
 //   delay(1000);
 // }
 
-#include <LiquidCrystal.h> // کتابخانه استاندارد LCD (برای LCD بدون I2C)
+#include <LiquidCrystal.h>  // کتابخانه استاندارد LCD (برای LCD بدون I2C)
 
 // تعریف پین های LCD: (RS, E, DB4, DB5, DB6, DB7)
-const int rsPin = 12; 
+const int rsPin = 12;
 const int enPin = 11;
 const int d4Pin = 5;
 const int d5Pin = 4;
@@ -77,15 +77,16 @@ const int d7Pin = 2;
 LiquidCrystal lcd(rsPin, enPin, d4Pin, d5Pin, d6Pin, d7Pin);
 
 // تعریف پین ها و متغیرهای دیگر پروژه
-const int lm35Pin = A0;      
-const int fanPWMPin = 9;     
+const int lm35Pin = A0;
+const int fanPWMPin = 9;
 float temperatureC = 0;
 int fanPWMValue = 0;
-const int minTemp = 20;     
-const int maxTemp = 50;     
+const int minTemp = 20;
+const int maxTemp = 50;
+const int buzzerPin = 6;
 // متغیرها برای ساعت شمار فن
-unsigned long totalDeviceOnTimeSeconds = 0; // زمان روشن بودن فن اول بر حسب ثانیه
-unsigned long fanOnTimeSeconds = 0; // زمان روشن بودن فن دوم بر حسب ثانیه
+unsigned long totalDeviceOnTimeSeconds = 0;  // زمان روشن بودن فن اول بر حسب ثانیه
+unsigned long fanOnTimeSeconds = 0;          // زمان روشن بودن فن دوم بر حسب ثانیه
 // متغیر برای اندازه گیری زمان سپری شده در هر حلقه
 unsigned long previousMillis = 0;
 unsigned long currentMillis = 0;
@@ -94,24 +95,44 @@ unsigned long loopDuration = 0;
 // متغیرهای جدید برای نمایش غیرمسدودکننده پیام
 unsigned long messageDisplayStartTime = 0;
 bool showMessage = false;
-const unsigned long messageDuration = 2000; // مدت زمان نمایش پیام (2 ثانیه)
+const unsigned long messageDuration = 2000;  // مدت زمان نمایش پیام (2 ثانیه)
 
 const char* fanStatusMessages[] = {
-  " Now it's coool   Here you are  ",        // این پیام با ایندکس 0 فراخوانی می شود
-  "It's a bit hotttLet's cool down!",         // این پیام با ایندکس 1 فراخوانی می شود
-  "Fan at MAX speed just god helps"   // این پیام با ایندکس 2 فراخوانی می شود
+  " Now it's coool   Here you are  ",  // این پیام با ایندکس 0 فراخوانی می شود
+  "It's a bit hotttLet's cool down!",  // این پیام با ایندکس 1 فراخوانی می شود
+  "Fan at MAX speed just god helps"    // این پیام با ایندکس 2 فراخوانی می شود
 };
 
 // متغیر جدید برای ذخیره وضعیت قبلی فن
-int previousFanState = -1; // -1 به معنی وضعیت نامشخص در ابتدا
+int previousFanState = -1;  // -1 به معنی وضعیت نامشخص در ابتدا
 int currentFanState = -1;
+
+unsigned long lastBeepTime = 0;
+const unsigned long beepDuration = 200;  // مدت زمان هر بوق (200 میلی ثانیه)
+int beepsToMake = 0;                     // تعداد بوق های باقی مانده
+bool isBeeping = false;                  // پرچم برای فعال بودن بوق زدن
+
+// تابع کمکی برای زمان بندی غیرمسدودکننده
+bool isTimeUp(unsigned long& lastExecutionTime, unsigned long interval) {
+  if (millis() - lastExecutionTime >= interval) {
+    lastExecutionTime = millis();
+    return true;
+  }
+  return false;
+}
+
+// متغیرهای جداگانه برای هر تایمر
+unsigned long lastDisplayUpdateTime = 0;
+unsigned long lastSerialPrintTime = 0;
 
 void setup() {
   pinMode(fanPWMPin, OUTPUT);
   analogWrite(fanPWMPin, 0);
+  pinMode(buzzerPin, OUTPUT);
+  digitalWrite(buzzerPin, LOW);
   Serial.begin(9600);
 
-  lcd.begin(16, 2); 
+  lcd.begin(16, 2);
   lcd.print("Device is ON");
   lcd.setCursor(0, 1);
   lcd.print("Starting...");
@@ -139,20 +160,25 @@ void loop() {
   } else if (temperatureC >= minTemp && temperatureC <= maxTemp) {
     fanPWMValue = map(temperatureC, minTemp, maxTemp, 0, 255);
     currentFanState = 1;
-  } else { // temperatureC > maxTemp
+  } else {  // temperatureC > maxTemp
     fanPWMValue = 255;
     currentFanState = 2;
   }
-  
+
   analogWrite(fanPWMPin, fanPWMValue);
 
-if (currentFanState != previousFanState) {
+  if (currentFanState != previousFanState) {
     showMessage = true;
     messageDisplayStartTime = currentMillis;
-    lcd.clear(); // پاک کردن صفحه برای پیام جدید
+    lcd.clear();  // پاک کردن صفحه برای پیام جدید
+
+    if (currentFanState > 0) {        // بوق زدن فقط برای حالت های ON
+      beepsToMake = currentFanState;  // تعداد بوق ها برابر با وضعیت فعلی (1 یا 2)
+      isBeeping = true;
+    }
   }
 
- // به روزرسانی وضعیت قبلی برای حلقه بعدی
+  // به روزرسانی وضعیت قبلی برای حلقه بعدی
   previousFanState = currentFanState;
 
   //  منطق ساعت شمار فن با شرط صفر شدن
@@ -167,64 +193,113 @@ if (currentFanState != previousFanState) {
     fanOnTimeSeconds += loopDuration / 1000;
   }
 
-if (showMessage && (currentMillis - messageDisplayStartTime < messageDuration)) {
+  if (isBeeping) {
+    if (isTimeUp(lastBeepTime, beepDuration)) {
+      if (digitalRead(buzzerPin) == LOW) {
+        // اگر بازر خاموش است، آن را روشن کن
+        digitalWrite(buzzerPin, HIGH);
+      } else {
+        // اگر بازر روشن است، آن را خاموش کن و تعداد بوق ها را کم کن
+        digitalWrite(buzzerPin, LOW);
+        beepsToMake--;
+        if (beepsToMake <= 0) {
+          // اگر بوق ها تمام شدند، فرآیند را متوقف کن
+          isBeeping = false;
+        }
+      }
+    }
+  }
+  if (isTimeUp(lastDisplayUpdateTime, 100)) {
+    if (showMessage && (currentMillis - messageDisplayStartTime < messageDuration)) {
       // اگر هنوز در بازه 2 ثانیه نمایش پیام هستیم
       lcd.setCursor(0, 0);
       // lcd.print(fanStatusMessages[currentFanState]);
       // if the 2nd half didn't print:
-      for(int i=0;i<32;i++){
-        if(i == 16)
-          lcd.setCursor(0, 1);
-        lcd.print(fanStatusMessages[currentFanState][i]);
-      }
-  }else {
+      // for (int i = 0; i < 32; i++) {
+      //   if (i == 16)
+      //     lcd.setCursor(0, 1);
+      //   lcd.print(fanStatusMessages[currentFanState][i]);
+      // }
+      printLooongStrings(fanStatusMessages, currentFanState);
+
+    } else {
       // اگر زمان نمایش پیام به پایان رسیده یا پیامی برای نمایش نیست
-      if (showMessage) { // فقط یک بار پس از اتمام زمان پیام
-          lcd.clear();
-          showMessage = false;
+      if (showMessage) {  // فقط یک بار پس از اتمام زمان پیام
+        lcd.clear();
+        showMessage = false;
       }
- // نمایش دما روی LCD
-  lcd.setCursor(0, 0);
-  lcd.print("Temp: ");
-  lcd.print(temperatureC, 2); // نمایش دما با یک رقم اعشار
-  lcd.print((char)223);       // کاراکتر درجه (°)
-  lcd.print("C");           // برای پاک کردن هر چیز قبلی
+      // نمایش دما روی LCD
+      lcd.setCursor(0, 0);
+      lcd.print("Temp: ");
+      lcd.print(temperatureC, 2);  // نمایش دما با یک رقم اعشار
+      lcd.print((char)223);        // کاراکتر درجه (°)
+      lcd.print("C");              // برای پاک کردن هر چیز قبلی
 
-  // 4. محاسبه زمان کلی دستگاه از روی millis()
-  totalDeviceOnTimeSeconds = currentMillis / 1000;
+      // 4. محاسبه زمان کلی دستگاه از روی millis()
+      totalDeviceOnTimeSeconds = currentMillis / 1000;
 
-  // 5. نمایش اطلاعات روی LCD
-  // نمایش زمان روشن بودن دستگاه (ساعت اول)
-  lcd.setCursor(0, 1);
-  lcd.print(totalDeviceOnTimeSeconds / 3600);         // ساعت
-  lcd.print(":");
-  lcd.print((totalDeviceOnTimeSeconds % 3600) / 60);  // دقیقه
-  lcd.print(":");
-  lcd.print(totalDeviceOnTimeSeconds % 60);           // ثانیه
-  
-  // setting distance between 2 clocks
-  if((totalDeviceOnTimeSeconds / 3600) < 10 && (fanOnTimeSeconds / 3600) < 10)
-    lcd.print("  ");
-  else if((totalDeviceOnTimeSeconds / 3600) >= 10 && (fanOnTimeSeconds / 3600) < 10)
-    lcd.print(" ");
+      // 5. نمایش اطلاعات روی LCD
+      // نمایش زمان روشن بودن دستگاه (ساعت اول)
+      lcd.setCursor(0, 1);
+      lcd.print(totalDeviceOnTimeSeconds / 3600);  // ساعت
+      lcd.print(":");
+      lcd.print((totalDeviceOnTimeSeconds % 3600) / 60);  // دقیقه
+      lcd.print(":");
+      lcd.print(totalDeviceOnTimeSeconds % 60);  // ثانیه
 
-  // نمایش زمان روشن بودن فن (ساعت دوم)
-  lcd.print(fanOnTimeSeconds / 3600);         // ساعت
-  lcd.print(":");
-  lcd.print((fanOnTimeSeconds % 3600) / 60);  // دقیقه
-  lcd.print(":");
-  lcd.print(fanOnTimeSeconds % 60);           // ثانیه
-  // lcd.print(loopDuration);
+      // setting distance between 2 clocks
+      if ((totalDeviceOnTimeSeconds / 3600) < 10 && (fanOnTimeSeconds / 3600) < 10)
+        lcd.print("  ");
+      else if ((totalDeviceOnTimeSeconds / 3600) >= 10 && (fanOnTimeSeconds / 3600) < 10)
+        lcd.print(" ");
+
+      // نمایش زمان روشن بودن فن (ساعت دوم)
+      lcd.print(fanOnTimeSeconds / 3600);  // ساعت
+      lcd.print(":");
+      lcd.print((fanOnTimeSeconds % 3600) / 60);  // دقیقه
+      lcd.print(":");
+      lcd.print(fanOnTimeSeconds % 60);  // ثانیه
+      // lcd.print(loopDuration);
+    }
   }
   // 6. نمایش دما و PWM روی سریال مانیتور (برای اشکال زدایی)
-  Serial.print("Temp: ");
-  Serial.print(temperatureC);
-  Serial.print("C | Fan PWM: ");
-  Serial.print(fanPWMValue);
-  Serial.print(" | Fan On Time: ");
-  Serial.print(fanOnTimeSeconds);
-  Serial.println("s");
-
+  if (isTimeUp(lastSerialPrintTime, 1000)) {
+    Serial.print("Temp: ");
+    Serial.print(temperatureC);
+    Serial.print("C | Fan PWM: ");
+    Serial.print(fanPWMValue);
+    Serial.print(" | Fan On Time: ");
+    Serial.print(fanOnTimeSeconds);
+    Serial.println("s");
+  }
   // تأخیر کوتاه برای جلوگیری از نوسانات شدید نمایش
-  delay(100); 
+  // delay(100);
+}
+
+void printLooongStrings1(const char* str, int index) {
+  for (int i = 0; i < 32; i++) {
+    if (i == 16)
+      lcd.setCursor(0, 1);
+    // lcd.print(*str[index][i]);
+  }
+}
+
+void printLooongStrings(const char* strings[], int index) {
+  // رشته مورد نظر را از آرایه انتخاب می کنیم
+  const char* str = strings[index]; 
+  
+  for (int i = 0; i < 32; i++) {
+    // بررسی می کنیم که به انتهای رشته نرسیده باشیم
+    if (str[i] == '\0') {
+      break; 
+    }
+    
+    // موقعیت مکان نما را برای خط دوم تنظیم می کنیم
+    if (i == 16) {
+      lcd.setCursor(0, 1);
+    }
+    
+    // هر کاراکتر را به صورت جداگانه چاپ می کنیم
+    lcd.print(str[i]);
+  }
 }
